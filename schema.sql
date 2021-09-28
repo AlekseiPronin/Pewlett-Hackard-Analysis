0768d2d1d4fce0495a6e8c6aa8a120c94e8222aa
Pronin <alexpronin.dm@gmail.com>
2021-09-28 14:25:58 +0300

    adding the schema

--- schema.sql
+++ schema.sql
@@ -41,7 +41,8 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles(
@@ -49,9 +50,12 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
+
+DROP TABLE titles;
+
+DROP TABLE dept_emp;
